2b301514721380cad92d72910d8fc5fd7fb9cbb5
LALITH M S <mslalithlalith@gmail.com>
2026-01-30 15:06:31 +0530

    problem 25

--- problem_25.py
+++ problem_25.py
@@ -1,8 +1,8 @@
-# Problem 25: Find GCD of two numbers
+ Problem 25: Find GCD of two numbers
 # Find and fix the error
 
 def gcd(a, b):
-    while b != 0:
+    while b !#= 0:
         temp = b
         b = a % b
         a = temp
